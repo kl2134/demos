4b25375ee28f42520258242bf4f237a8520e53ba
KL <klin2134@outlook.com>
2019-05-23 11:49:29 +0630

    Initial commit  of LinkedList and DoublyLinkedList Implementations

--- algorithms/data_structures/node.py
+++ algorithms/data_structures/node.py
@@ -1,13 +1,30 @@
 class BaseNode(object):
 
     def __init__(self, value):
-        self.value = value
+        self._value = value
+
+    def getValue(self):
+        return self._value
+
+    def setValue(self, value):
+        self._value = value
 
 class GraphNode(BaseNode):
 
     def __init__(self, value):
         BaseNode.__init__(self, value)
-        self.neighbors = []
+        self._neighbors = []
+
+    def getNeighbors(self):
+        return self._neighbors
+
+    def addNeighbor(self, neighbor):
+        if not neighbor in self._neighbors:
+            self._neighbors.append(neighbor)
+
+    def deleteNeighbor(self, neighbor):
+        if neighbor in self._neighbors:
+            self._neighbors.remove(neighbor)
 
 class LinkedListNode(BaseNode):
 
@@ -31,23 +48,36 @@ class DoublyLinkedListNode(LinkedListNode):
 if __name__ == '__main__':
 
     baseNode = BaseNode(1)
-    assert baseNode.value == 1
+    assert baseNode._value == 1
+    assert baseNode.getValue() == 1
+    baseNode.setValue(2)
+    assert baseNode.getValue() == 2
 
     gNode = GraphNode(1)
-    assert gNode.value == 1
-    assert gNode.neighbors == []
+    assert gNode._value == 1
+    assert gNode._neighbors == []
+    assert gNode.getNeighbors() == []
+
+    gNode.addNeighbor(34)
+    assert gNode.getNeighbors() == [34]
+
+    gNode.deleteNeighbor(3)
+    assert gNode.getNeighbors() == [34]
+
+    gNode.deleteNeighbor(34)
+    assert gNode.getNeighbors() == []
 
     llNode = LinkedListNode(1)
-    assert llNode.value == 1
+    assert llNode._value == 1
     assert llNode.next == None
 
     dllNode = DoublyLinkedListNode(1)
-    assert dllNode.value == 1
+    assert dllNode._value == 1
     assert dllNode.next == None
     assert dllNode.previous == None
 
     tNode = BinaryTreeNode(1)
-    assert tNode.value == 1
+    assert tNode._value == 1
     assert tNode.left == None
     assert tNode.right == None
 
